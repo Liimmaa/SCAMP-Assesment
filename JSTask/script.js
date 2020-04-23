0ac339018af64c87a96e3bf91196d35ef663451c
Lima Mmegwa <halimammegwa@gmail.com>
2020-04-23 19:38:25 +0100

    Outputs the fibbonacci sequence of a given number

---
+++ JSTask/script.js
@@ -0,0 +1,12 @@
+function fibbonacci(n) {
+    var a = 0;
+    var b = 1;
+    var f = 1;
+
+    for (var i = 2; i<=n; i++){
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    return f;
+}  
